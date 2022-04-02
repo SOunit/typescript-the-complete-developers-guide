2448b10d157fb601fea1f8d801c3183b4a27ba84
SOunit <workbookwork7@gmail.com>
2022-04-02 13:39:27 -0700

    add interface for poor type definition

--- 14_express_ts/src/index.ts
+++ 14_express_ts/src/index.ts
@@ -1,6 +1,13 @@
 import express from "express";
+import { router } from "./routes/loginRoutes";
+import { urlencoded } from "body-parser";
 
 const app = express();
+
+// to use post and req.body
+app.use(urlencoded({ extended: true }));
+
+app.use(router);
 
 const port = 5000;
 app.listen(port, () => {
